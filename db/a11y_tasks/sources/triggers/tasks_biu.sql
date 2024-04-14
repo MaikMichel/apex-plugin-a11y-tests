b457b9d16c9db40839ba2e53a491634c53dd4124
Maik Michel <maik.michel78@gmail.com>
2024-04-14 14:07:33 +0200

    Fixed trigger because APEX comes with an ID when creating a new record

--- db/a11y_tasks/sources/triggers/tasks_biu.sql
+++ db/a11y_tasks/sources/triggers/tasks_biu.sql
@@ -1,7 +1,7 @@
 create or replace trigger tasks_biu
 	before insert or update on tasks for each row
 begin
-	if :new.id is null then
+	if inserting then
 		:new.id := to_number(sys_guid(), 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 	end if;
 end tasks_biu;
