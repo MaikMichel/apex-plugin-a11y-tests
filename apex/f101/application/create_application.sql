b13e63fbe93ab2a13121c7d0f7082b8ab6871bf6
Maik Michel <maik.michel78@gmail.com>
2024-11-13 21:08:09 +0100

    Change: Update plugin

--- apex/f101/application/create_application.sql
+++ apex/f101/application/create_application.sql
@@ -30,7 +30,7 @@ wwv_imp_workspace.create_flow(
 ,p_timestamp_tz_format=>'DS'
 ,p_direction_right_to_left=>'N'
 ,p_flow_image_prefix => nvl(wwv_flow_application_install.get_image_prefix,'')
-,p_authentication_id=>wwv_flow_imp.id(10761661073483499)
+,p_authentication_id=>wwv_flow_imp.id(11919190647754967)
 ,p_application_tab_set=>1
 ,p_logo_type=>'T'
 ,p_logo_text=>'Tasks'
